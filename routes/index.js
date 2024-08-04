85e501ff1099b41dbea8206dad373dd374b3c10d
Karam <70372614+abcd-hud@users.noreply.github.com>
2024-08-04 12:37:07 +0500

    almost all the routes have been setup

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/', function (req, res) {
-    res.send('API is running currently in index.js!');
+const incomeModel = require('../models/incomeModel');
+const expensesModel = require('../models/expensesModel');
+router.get('/', async function (req, res) {
+    let total_expenses = 0,total_income = 0 , netTotal = 0;
+    const incomes =await incomeModel.find({});
+    const expenses =await expensesModel.find({});
+    incomes.map((income)=>total_income += income.amount);
+    expenses.map((expense)=>total_expenses += expense.amount);
+    netTotal = total_income - total_expenses;
+    res.send({netTotal,total_expenses,total_income,incomes,expenses});
 });
 
 module.exports = router;
